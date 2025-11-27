029542c23a6b9b2db3c6c73d93e17b0e670ba117
beknazaarr <beknazaarr@gmail.com>
2025-11-27 07:11:55 +0600

    kfnskd

--- backend-java/src/main/java/org/example/backendjava/booking_to_doctore_service/exception/DoctorAlreadyBookedException.java
+++
@@ -1,7 +0,0 @@
-package org.example.backendjava.booking_to_doctore_service.exception;
-
-public class DoctorAlreadyBookedException extends RuntimeException {
-    public DoctorAlreadyBookedException(String message) {
-        super(message);
-    }
-}
--- backend-java/src/main/java/org/example/backendjava/booking_to_doctore_service/exception/PatientNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package org.example.backendjava.booking_to_doctore_service.exception;
-
-public class PatientNotFoundException extends RuntimeException {
-    public PatientNotFoundException(String message) {
-        super(message);
-    }
-}
