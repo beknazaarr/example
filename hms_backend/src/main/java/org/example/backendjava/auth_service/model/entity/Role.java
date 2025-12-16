0c870e7bd6685f34f5e1b5c7b8cf98fc138dae80
beknazaarr <beknazaarr@gmail.com>
2025-12-17 03:15:00 +0600

    frfenj

--- hms_backend/src/main/java/org/example/backendjava/auth_service/model/entity/Role.java
+++
@@ -1,5 +0,0 @@
-package org.example.backendjava.auth_service.model.entity;
-
-public enum Role {
-    USER, ADMIN, DOCTOR
-}
--- hms_backend/src/main/java/org/example/backendjava/auth_service/userexception/UsernameAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.example.backendjava.auth_service.userexception;
-
-public class UsernameAlreadyExistsException extends RuntimeException {
-    public UsernameAlreadyExistsException(String message) {
-        super(message);
-    }
-}
